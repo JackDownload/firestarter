534e1970fbffd43a3fa924f40e585af7a224b4f6
JackDownload <kismocsy@gmail.com>
2020-02-09 18:24:18 +0100

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul v-for="(blogPost, index) in blogPosts" :key="index">
-      <nuxt-link :to="`blog/${blogPost.slug}`">{{blogPost.title}} és {{blogPost.category}}</nuxt-link>
+      <nuxt-link :to="`blog/${blogPost.slug}`">{{blogPost.title}} és {{blogPost.blogcategories}} és {{blogPost.categories}}</nuxt-link>
       <p>{{blogPost.description}}</p>
     </ul>
   </div>
